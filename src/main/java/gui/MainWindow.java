873ad377d0a56285d41fd1ada70f70e09c45289e
Jean Petric <petricomp@gmail.com>
2017-03-18 12:42:20 +0000

    moving configuration to main window

--- src/main/java/gui/MainWindow.java
+++ src/main/java/gui/MainWindow.java
@@ -3,13 +3,17 @@ package gui;
 import engine.Track;
 import indicator.BasicDrumTab;
 import indicator.DrumTab;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
@@ -31,9 +35,27 @@ public class MainWindow extends Window {
     Rectangle tabCursor;
     @FXML
     TextField tempo;
+    @FXML
+    ChoiceBox playerMode;
 
     private AbstractModePlayer player = null;
     private Track track = null;
+    private final int RUNNING = 0;
+    private final int STATIC = 1;
+    private int newPlayerMode = -1;
+
+    @FXML
+    public void initialize() {
+        String playerModeProperty = DrummerProperties.getProperty("playerMode");
+        playerMode.getSelectionModel().select(playerModeProperty.equals("running") ? RUNNING : STATIC);
+        playerMode.getSelectionModel().selectedIndexProperty().addListener(new ChangeListener<Number>() {
+            @Override
+            public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
+                newPlayerMode = newValue.intValue();
+                setPlayerMode();
+            }
+        });
+    }
 
     public void load(ActionEvent actionEvent) throws IOException {
         FileChooser tabFile = new FileChooser();
@@ -98,5 +120,15 @@ public class MainWindow extends Window {
         } else {
             player = new StaticModePlayer(tabArea, tabCursor, tempo, track);
         }
+    }
+
+    public void setPlayerMode() {
+        String result = "";
+        if (newPlayerMode == 0) {
+            result = "running";
+        } else {
+            result = "static";
+        }
+        DrummerProperties.saveProperty("playerMode", result);
     }
 }
