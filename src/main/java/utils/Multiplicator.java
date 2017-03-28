0a419d4fa500f2dcaa3672a506416d5f4c0888ac
Jean Petric <petricomp@gmail.com>
2017-03-28 18:13:44 +0100

    adding random stuff

---
+++ src/main/java/utils/Multiplicator.java
@@ -0,0 +1,11 @@
+package utils;
+
+/**
+ * Created by jean on 28/03/17.
+ */
+public class Multiplicator {
+    public int multiplicator(int a) {
+        int result = a * 2;
+        return result;
+    }
+}
---
+++ src/test/java/utils/MultiplicatorTest.java
@@ -0,0 +1,18 @@
+package utils;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by jean on 28/03/17.
+ */
+public class MultiplicatorTest {
+    @Test
+    public void multiplicator() throws Exception {
+        Multiplicator m = new Multiplicator();
+        int a = m.multiplicator(10);
+        assertEquals(20, a);
+    }
+
+}
