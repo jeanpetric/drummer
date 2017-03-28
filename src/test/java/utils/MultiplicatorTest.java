11d16deb9298923ce1b99a73d349efaf12883e58
Jean Petric <petricomp@gmail.com>
2017-03-28 18:14:16 +0100

    minor

--- src/test/java/utils/MultiplicatorTest.java
+++ src/test/java/utils/MultiplicatorTest.java
@@ -8,6 +8,7 @@ import static org.junit.Assert.*;
  * Created by jean on 28/03/17.
  */
 public class MultiplicatorTest {
+
     @Test
     public void multiplicator() throws Exception {
         Multiplicator m = new Multiplicator();
