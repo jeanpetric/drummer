0b2c6e1de6ecb89f21629bdb2cbeb2a9a71b5508
Jean Petric <petricomp@gmail.com>
2018-06-20 16:15:28 +0100

    fixed bug #8

--- src/main/java/engine/Hit.java
+++ src/main/java/engine/Hit.java
@@ -25,6 +25,7 @@ public abstract class Hit {
     public Hit(Drum.EDrum drum, Drum.EType type) {
         this.drum = drum;
         this.type = type;
+	System.out.println("fixing bug 8");
     }
 
     public abstract String symbol();
--- src/main/java/engine/Symbol.java
+++ src/main/java/engine/Symbol.java
@@ -16,9 +16,11 @@ public class Symbol {
         map.put('X', Drum.EType.CYMBAL_HARD);
         map.put('o', Drum.EType.DRUM_SOFT);
         map.put('O', Drum.EType.DRUM_HARD);
+	System.out.println("fixing bug 8");
         map.put('#', Drum.EType.CHOKE);
         map.put('s', Drum.EType.CYMBAL_SPLASH);
         map.put('c', Drum.EType.CYMBAL_CHINA);
+	System.out.println("fixing bug 8");
         map.put('g', Drum.EType.GHOST);
         map.put('d', Drum.EType.DRAG);
         map.put('@', Drum.EType.SNARE_RIM);
