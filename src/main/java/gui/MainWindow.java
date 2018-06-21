603b924321519d67ef636cd407449e9a3d7e797d
Jean Petric <petricomp@gmail.com>
2018-06-21 14:33:07 +0100

    fixed bug #13

--- src/main/java/gui/MainWindow.java
+++ src/main/java/gui/MainWindow.java
@@ -83,10 +83,7 @@ public class MainWindow extends Window {
 
     public void reset(ActionEvent actionEvent) {
         player.reset();
-        System.out.println("some changes");
-        
-        System.out.println("some changes");
-        System.out.println("fault inserted here");
+        System.out.println("fault fixed here");
     }
 
     public void adjustSize(Stage stage) {
